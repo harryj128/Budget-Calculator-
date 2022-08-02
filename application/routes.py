6920a1e724b6875123d77e6a59ca986a1d0dd83c
Harry Johnson <harryj128@gmail.com>
2022-08-02 16:33:13 +0000

    Finshed Templates and forms/ Started on the routes

--- application/routes.py
+++ application/routes.py
@@ -6,15 +6,35 @@ from flask import url_for, request, redirect, render_template
 
 #C - User
 #R - User
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users', users = users)
+
 #U - User
 #D - User
 
 #C - Budget
 #R - Budget
+@app.route('/budgets')
+def budgets():
+    budgets = Budget.query.all()
+    return render_template('budgets', budgets = budgets)
+
+@app.route('/budgets/<int:user_id>')
+def budgets_specific(user_id):
+    # need a line here to query the db for all budgets linked to a certain user
+    budgets = Budget.query.get(user_id)
+    return render_template('budgets', budgets = budgets)
 #U - Budget
 #D - Budget
 
-#C - Task
-#R - Task
-#U - Task
-#D - Task
+#C - Items
+#R - Items
+@app.route('/items/<int:budget_id>')
+def budgets_specific(budget_id):
+     # need a line here to query the db for all items linked to a certain budget
+    items = Item.query.get(budget_id)
+    return render_template('items', items = items)
+#U - Items
+#D - Items
